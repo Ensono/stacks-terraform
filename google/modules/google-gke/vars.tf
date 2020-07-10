4997b0faf8716c27cb8a79be21ea7089661076ba
Saul France <saul.france@amido.com>
2020-07-10 09:59:24 +0100

    fix: updated cluster version in module

--- google/modules/google-gke/vars.tf
+++ google/modules/google-gke/vars.tf
@@ -151,7 +151,7 @@ variable "enable_legacy_abac" {
 
 variable "cluster_version" {
   type    = string
-  default = "1.15.11-gke.12"
+  default = "1.15.11-gke.17"
 }
 
 variable "is_cluster_private" {
