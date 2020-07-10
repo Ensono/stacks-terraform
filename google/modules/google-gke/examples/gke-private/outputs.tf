11cf875d793e4ad58d52d9f5f830a61e6eade178
Saul France <saul.france@amido.com>
2020-07-10 11:00:56 +0100

    chore: updated examples to use gke_cluster module name

--- google/modules/google-gke/examples/gke-private/outputs.tf
+++ google/modules/google-gke/examples/gke-private/outputs.tf
@@ -2,51 +2,51 @@ output "cluster_name" {
   # This may seem redundant with the `name` input, but it serves an important
   # purpose. Terraform won't establish a dependency graph without this to interpolate on.
   description = "The name of the cluster master. This output is used for interpolation with node pools, other modules."
-  value       = module.gke-private.cluster_name
+  value       = module.gke_cluster.cluster_name
 }
 
 output "master_version" {
   description = "The Kubernetes master version."
-  value       = module.gke-private.master_version
+  value       = module.gke_cluster.master_version
 }
 
 output "cluster_endpoint" {
   description = "The IP address of the cluster master."
   sensitive   = true
-  value       = module.gke-private.cluster_endpoint
+  value       = module.gke_cluster.cluster_endpoint
 }
 
 # output "client_certificate" {
 #   description = "Public certificate used by clients to authenticate to the cluster endpoint."
 #   sensitive   = true
-#   value       = module.gke-private.client_certificate
+#   value       = module.gke_cluster.client_certificate
 # }
 
 output "client_key" {
   description = "Private key used by clients to authenticate to the cluster endpoint."
   sensitive   = true
-  value       = module.gke-private.client_key
+  value       = module.gke_cluster.client_key
 }
 
 output "cluster_ca_certificate" {
   description = "The public certificate that is the root of trust for the cluster."
   sensitive   = true
-  value       = module.gke-private.cluster_ca_certificate
+  value       = module.gke_cluster.cluster_ca_certificate
 }
 
 output "gke_ingress_public_ip" {
   description = "Public IP to be used for the ingress controller inside the cluster"
-  value       = module.gke-private.gke_ingress_public_ip
+  value       = module.gke_cluster.gke_ingress_public_ip
 }
 
 output "gke_ingress_public_ip_name" {
   description = "Public IP to be used for the ingress controller inside the cluster"
-  value       = module.gke-private.gke_ingress_public_ip_name
+  value       = module.gke_cluster.gke_ingress_public_ip_name
 }
 
 # output "gke_ingress_private_ip" {
 #   description = "Public IP to be used for the ingress controller inside the cluster"
-#   value       = module.gke-private.gke_ingress_private_ip
+#   value       = module.gke_cluster.gke_ingress_private_ip
 # }
 
 output "project_id" {
