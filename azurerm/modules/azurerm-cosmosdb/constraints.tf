453294ffb987d4ed55aad6dd1ae5c18e9eb46620
Jack Blower <Jack@elvenspellmaker.co.uk>
2021-06-11 10:35:15 +0100

    fix: removes unneeded constraints

--- azurerm/modules/azurerm-cosmosdb/constraints.tf
+++ azurerm/modules/azurerm-cosmosdb/constraints.tf
@@ -5,13 +5,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 2.5"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 3.1"
-    }
   }
 }
