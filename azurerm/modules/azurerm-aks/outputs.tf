f1d9497826908e685dd1a2fd269052aa26f3f662
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2022-01-25 15:03:18 +0000

    Added new outputs for resources
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- azurerm/modules/azurerm-aks/outputs.tf
+++ azurerm/modules/azurerm-aks/outputs.tf
@@ -89,3 +89,35 @@ output "aks_ingress_public_ip" {
 output "key_vault_name" {
   value = azurerm_key_vault.default.0.name
 }
+
+
+#########################################
+# Application Insights
+#########################################
+
+output "app_insights_resource_group_name" {
+  value = azurerm_log_analytics_workspace.default.resource_group_name
+}
+output "app_insights_name" {
+  value = azurerm_log_analytics_workspace.default.name
+}
+
+#########################################
+# DNS settings
+#########################################
+
+output "dns_resource_group_name" {
+  value = azurerm_dns_zone.default.resource_group_name
+}
+
+output "dns_internal_resource_group_name" {
+  value = azurerm_dns_zone.default.resource_group_name
+}
+
+output "dns_base_domain" {
+  value = azurerm_dns_zone.default.name
+}
+
+output "dns_base_domain_internal" {
+  value = azurerm_private_dns_zone.default.name
+}
