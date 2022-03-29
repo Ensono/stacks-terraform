b24324a0eaec1d4df99612204651d3846976efa0
Dibya Dhar <dibya.dhar@amido.com>
2022-03-29 21:10:17 +0100

    Add EKS IRSA Module

--- aws/modules/infrastructure_modules/eks/outputs.tf
+++ aws/modules/infrastructure_modules/eks/outputs.tf
@@ -32,3 +32,8 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.cluster_name
 }
+
+output "cluster" {
+  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+  value       = module.eks.cluster_oidc_issuer_url
+}
