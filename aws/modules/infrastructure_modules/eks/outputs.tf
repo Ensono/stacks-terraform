daddb18ca39d2f8974c93d901acfe74283ee59b0
Dibya Dhar <dibya.dhar@amido.com>
2022-03-29 21:20:44 +0100

    Update oidc output name

--- aws/modules/infrastructure_modules/eks/outputs.tf
+++ aws/modules/infrastructure_modules/eks/outputs.tf
@@ -33,7 +33,7 @@ output "cluster_name" {
   value       = local.cluster_name
 }
 
-output "cluster" {
+output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
   value       = module.eks.cluster_oidc_issuer_url
 }
