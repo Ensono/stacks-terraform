9e0f7da106842f6cb0174cd84958979eb901053e
Dibya Dhar <dibya.dhar@amido.com>
2022-03-29 22:43:14 +0100

    Add cluster certificate data

--- aws/modules/infrastructure_modules/eks/outputs.tf
+++ aws/modules/infrastructure_modules/eks/outputs.tf
@@ -37,3 +37,8 @@ output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
   value       = module.eks.cluster_oidc_issuer_url
 }
+
+output "cluster_certificate_authority_data" {
+  description = "base64 encoded certificate data required to communicate with your cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
