6af9b261be998a096b4cac738babb4c4658c03a9
Dibya Dhar <dibya.dhar@amido.com>
2022-04-26 13:56:13 +0100

    format file and gitignore

--- aws/modules/infrastructure_modules/stacks_app/app.tf
+++ aws/modules/infrastructure_modules/stacks_app/app.tf
@@ -2,17 +2,17 @@
 module "zones" {
   source  = "terraform-aws-modules/route53/aws//modules/zones"
   version = "~> 2.0"
-  
-  count   = var.enable_zone
-  zones   = var.public_zones
-  tags    = var.tags
+
+  count = var.enable_zone
+  zones = var.public_zones
+  tags  = var.tags
 }
 
 # DynamoDB
 module "dynamodb_table" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
-  version  = "~> 1.2"
-  
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "~> 1.2"
+
   count    = var.enable_dynamodb
   name     = var.table_name
   hash_key = var.hash_key
