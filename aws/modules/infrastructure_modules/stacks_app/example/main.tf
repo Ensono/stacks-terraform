e12eb87ed7403e3ef12b4815c0823273d72b0366
Dibya Dhar <dibya.dhar@amido.com>
2022-05-11 00:24:17 +0100

    fix: remove unwanted zones parameters

--- aws/modules/infrastructure_modules/stacks_app/example/main.tf
+++ aws/modules/infrastructure_modules/stacks_app/example/main.tf
@@ -9,12 +9,8 @@ resource "random_string" "random" {
 module "server_side_app" {
 
   source = "../"
-
-  enable_zone     = var.enable_zone ? 1 : 0
+  
   enable_dynamodb = var.enable_dynamodb ? 1 : 0
-  public_zones    = var.public_zones
-
-
   table_name     = "${var.table_name}-${random_string.random.result}"
   hash_key       = var.hash_key
   attribute_name = var.attribute_name
