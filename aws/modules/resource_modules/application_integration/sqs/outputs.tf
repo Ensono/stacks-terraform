c15aac4fcac5d3bc3bdb55e0127e829014ae38b4
wayerst <william.ayerst@amido.com>
2022-07-13 17:06:29 +0100

    Fixes output description

--- aws/modules/resource_modules/application_integration/sqs/outputs.tf
+++ aws/modules/resource_modules/application_integration/sqs/outputs.tf
@@ -3,11 +3,11 @@
 #########
 output "sqs_queue_id" {
   count       = var.create ? 1 : 0
-  description = "The ARN for the created Amazon SNS topic"
+  description = "The ID for the created Amazon SQS Queue"
   value       = var.create ? aws_sqs_queue.queue[0].id : null
 }
 output "sqs_queue_arn" {
   count       = var.create ? 1 : 0
-  description = "The ARN for the created Amazon SNS topic"
+  description = "The ARN for the created Amazon SQS Queue"
   value       = var.create ? aws_sqs_queue.queue[0].arn : null
 }
