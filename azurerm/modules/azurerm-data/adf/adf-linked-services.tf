5e026e23c71a9b6f19213d33640c399f68f86a03
James Horrocks <james.horrocks@live.co.uk>
2022-07-19 14:34:36 +0100

    edited the provider constraints

--- azurerm/modules/azurerm-data/adf/adf-linked-services.tf
+++ azurerm/modules/azurerm-data/adf/adf-linked-services.tf
@@ -1,6 +1,7 @@
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "default" {
   name                 = "ls_blob"
   data_factory_id      = azurerm_data_factory.default.id
+  resource_group_name  = var.resource_group_name
   service_endpoint     = var.default_storage_account_primary_blob_endpoint
   use_managed_identity = true
 
@@ -12,6 +13,7 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "default" {
 resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "adls_datalake" {
   name                 = "ls_adls_datalake"
   data_factory_id      = azurerm_data_factory.default.id
+  resource_group_name  = var.resource_group_name
   url                  = var.adls_storage_account_primary_dfs_endpoint
   use_managed_identity = true
 
