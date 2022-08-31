ae99d2bc524ee2e3ec3258a510400c2959c730b3
Francesco Calvino <francesco.calvino@amido.com>
2022-08-31 09:40:58 +0100

    deleted old folder

--- azurerm/modules/modulesfc/app/adf/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "ADF_ACCOUNT_NAME" {
-  description = "Azure Data Factory Name"
-  value       = azurerm_data_factory.default.name
-}
-
-output "ADF_FACTORY_ID" {
-  value = azurerm_data_factory.default.id
-}
--- azurerm/modules/modulesfc/app/mssql/sql-seed.tf
+++
@@ -1,5 +0,0 @@
-
-# Seed the test database
-locals {
-  sql_input_file = "${path.module}/../tests/input/sql/seed.sql"
-}
