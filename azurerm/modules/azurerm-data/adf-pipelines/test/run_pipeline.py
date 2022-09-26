ac239b0e2ef7e6113f40ce17fefb6a5437da8bb0
James Horrocks <james.horrocks@live.co.uk>
2022-09-26 13:15:46 +0100

    edited pipeline config files to include delta column and reformatted run_pipeline.py

--- azurerm/modules/azurerm-data/adf-pipelines/test/run_pipeline.py
+++ azurerm/modules/azurerm-data/adf-pipelines/test/run_pipeline.py
@@ -7,9 +7,7 @@ import os
 import time
 
 
-def create_run(
-    adf_client, resource_group_name, factory_name, pipeline_name, parameters
-):
+def create_run(adf_client, resource_group_name, factory_name, pipeline_name, parameters):
     run_id = adf_client.pipelines.create_run(
         resource_group_name, factory_name, pipeline_name, parameters=parameters
     )
@@ -35,7 +33,9 @@ def check_for_active_run(adf_client, resource_group_name, factory_name, pipeline
     print(pipeline_names)
     pipeline_name_filter = RunQueryFilter(operand="PipelineName", operator="In", values=pipeline_names)
 
-    filter_params = RunFilterParameters(last_updated_after=datetime.now() - timedelta(1), last_updated_before=datetime.now() + timedelta(1), filters = [status_filter, pipeline_name_filter])
+    filter_params = RunFilterParameters(last_updated_after=datetime.now() - timedelta(1),
+                                        last_updated_before=datetime.now() + timedelta(1),
+                                        filters=[status_filter, pipeline_name_filter])
 
     run_list = adf_client.pipeline_runs.query_by_factory(resource_group_name, factory_name, filter_params)
     run_list = run_list.value
@@ -48,10 +48,11 @@ def check_for_active_run(adf_client, resource_group_name, factory_name, pipeline
         return run_data.status, run_data.run_id
     else:
         return "No run", None
-        
+
 
 def run_adf_pipeline(
-    pipeline_parameters, pipeline_name, activity_data_bool=False, activity_name_filter="create_incident", check_for_runs_bool=False, dependant_pipelines=None
+        pipeline_parameters, pipeline_name, activity_data_bool=False, activity_name_filter="create_incident",
+        check_for_runs_bool=False, dependant_pipelines=None
 ):
     subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
     resource_group_name = os.environ["ADF_RESOURCE_GROUP_NAME"]
@@ -59,13 +60,14 @@ def run_adf_pipeline(
 
     credential = DefaultAzureCredential()
     adf_client = DataFactoryManagementClient(credential, subscription_id)
-    
+
     if check_for_runs_bool:
         if dependant_pipelines is None:
             dependant_pipelines = [pipeline_name]
         else:
             dependant_pipelines.append(pipeline_name)
-        existing_run_status, run_id = check_for_active_run(adf_client, resource_group_name, factory_name, dependant_pipelines)
+        existing_run_status, run_id = check_for_active_run(adf_client, resource_group_name, factory_name,
+                                                           dependant_pipelines)
         if existing_run_status == "No run":
             run_response = create_run(
                 adf_client, resource_group_name, factory_name, pipeline_name, pipeline_parameters
@@ -89,8 +91,10 @@ def run_adf_pipeline(
 
     if activity_data_bool:
         activity_name = RunQueryFilter(operand="ActivityName", operator="Equals", values=[activity_name_filter])
-        filter_params = RunFilterParameters(last_updated_after=datetime.now() - timedelta(1), last_updated_before=datetime.now() + timedelta(1), filters=[activity_name])
-        query_response = adf_client.activity_runs.query_by_pipeline_run(resource_group_name, factory_name, run_id, filter_params)
+        filter_params = RunFilterParameters(last_updated_after=datetime.now() - timedelta(1),
+                                            last_updated_before=datetime.now() + timedelta(1), filters=[activity_name])
+        query_response = adf_client.activity_runs.query_by_pipeline_run(resource_group_name, factory_name, run_id,
+                                                                        filter_params)
 
         return run_data, query_response
 
