63cf126df439b026a40548b32d05c0b9fc695a3c
jakub.slon <jakubslon@gmail.com>
2023-03-13 14:22:11 +0000

    fix/add-outputs

--- azurerm/modules/azurerm-adls/example/outputs.tf
+++ azurerm/modules/azurerm-adls/example/outputs.tf
@@ -9,3 +9,13 @@ output "storage_account_names" {
 output "storage_ids" {
   value = module.adls_default.storage_account_ids[*]
 }
+
+output "primary_blob_connection_string" {
+  value     = module.adls_default.primary_blob_connection_string[*]
+  sensitive = true
+}
+
+output "storage_account_primary_connection_string" {
+  value     = module.adls_default.storage_account_primary_connection_string[*]
+  sensitive = true
+}
--- azurerm/modules/azurerm-adls/outputs.tf
+++ azurerm/modules/azurerm-adls/outputs.tf
@@ -5,7 +5,7 @@
 # output "primary_blob_endpoint" {
 #   value = { for k, v in azurerm_storage_account.storage_account_default : k => v.primary_blob_endpoint }
 # }
- 
+
 
 output "storage_account_ids" {
   value = values(azurerm_storage_account.storage_account_default)[*].id
@@ -17,4 +17,14 @@ output "storage_account_names" {
 
 output "storage_account_id_1" {
   value = values(azurerm_storage_account.storage_account_default)[0].id
+}
+
+output "storage_account_primary_connection_string" {
+  value     = values(azurerm_storage_account.storage_account_default)[*].primary_connection_string
+  sensitive = true
+}
+
+output "primary_blob_connection_string" {
+  value     = values(azurerm_storage_account.storage_account_default)[*].primary_connection_string
+  sensitive = true
 }
