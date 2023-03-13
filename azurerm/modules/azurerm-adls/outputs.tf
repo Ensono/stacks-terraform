6c15f6fbabc16ef0c82e349e0d779171e839347c
“Rishikesh <rishikesh_singh@next.co.uk>
2023-03-13 12:07:37 +0000

    fixed:test output

--- azurerm/modules/azurerm-adls/outputs.tf
+++ azurerm/modules/azurerm-adls/outputs.tf
@@ -6,3 +6,16 @@
 #   value = { for k, v in azurerm_storage_account.storage_account_default : k => v.primary_blob_endpoint }
 # }
  
+
+ output "storage_account_ids" {
+   value =  { for i in azurerm_storage_account.storage_account_default : 
+   i.name => {
+    name = i.name
+    id = i.id
+   }}
+ }
+
+
+output "storage_account_ids_test" {
+  value = values(azurerm_storage_account.storage_account_default)[*].id
+}
