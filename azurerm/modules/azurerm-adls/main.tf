5851d5c49de685207c991c412c8435a9d32f7b38
jakub.slon <jakubslon@gmail.com>
2023-03-21 11:31:14 +0000

    fix/locals-fmt

--- azurerm/modules/azurerm-adls/main.tf
+++ azurerm/modules/azurerm-adls/main.tf
@@ -1,3 +1,8 @@
+locals {
+  containers_blob = flatten([for account_name, account_details in var.storage_account_details : [for container_name in account_details.containers_name : { name = container_name, account = account_name }] if account_details.hns_enabled != true])
+  containers_adls = flatten([for account_name, account_details in var.storage_account_details : [for container_name in account_details.containers_name : { name = container_name, account = account_name }] if account_details.hns_enabled == true])
+}
+
 resource "azurerm_storage_account" "storage_account_default" {
   for_each = var.storage_account_details
 
@@ -27,9 +32,4 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
   for_each           = { for i in toset(local.containers_adls) : i.name => i }
   name               = each.key
   storage_account_id = azurerm_storage_account.storage_account_default[each.value.account].id
-}
-
-locals {
-  containers_blob = flatten([for account_name, account_details in var.storage_account_details : [for container_name in account_details.containers_name : { name = container_name, account = account_name }] if account_details.hns_enabled != true])
-  containers_adls = flatten([for account_name, account_details in var.storage_account_details : [for container_name in account_details.containers_name : { name = container_name, account = account_name }] if account_details.hns_enabled == true])
 }
