18f5409b95726797217a7736981eae9d0f10ddd8
Satenderrathee <satender.rathee07@gmail.com>
2023-04-14 13:24:50 +0100

    added example

---
+++ azurerm/modules/azurerm-adb/example/main.tf
@@ -0,0 +1,30 @@
+data "azurerm_client_config" "current" {}
+
+module "default_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.25.0"
+  namespace  = "${var.name_company}-${var.name_project}"
+  stage      = var.stage
+  name       = "${lookup(var.location_name_map, var.resource_group_location, "uksouth")}-${var.name_component}"
+  attributes = var.attributes
+  delimiter  = "-"
+  tags       = var.tags
+}
+
+##################################################
+# ResourceGroups
+##################################################
+
+resource "azurerm_resource_group" "default" {
+  name     = module.default_label.id
+  location = var.resource_group_location
+  tags     = var.tags
+}
+
+module "adb" {
+  source                  = "../../azurerm-adb"
+  resource_namer          = module.default_label.id
+  resource_group_name     = azurerm_resource_group.default.name
+  resource_group_location = azurerm_resource_group.default.location
+  databricks_sku          = var.databricks_sku
+  resource_tags           = module.default_label.tags
+}
