31a3aeb68d1152f852095bbfcfa79fa64723b4bc
Satenderrathee <satender.rathee07@gmail.com>
2023-04-14 10:59:17 +0100

    added initial files

---
+++ azurerm/modules/azurerm-adb/var.tf
@@ -0,0 +1,46 @@
+############################################
+# NAMING
+############################################
+
+variable "resource_namer" {
+  type        = string
+  description = "User defined naming convention applied to all resources created as part of this module"
+}
+
+variable "resource_tags" {
+  description = "Map of tags to be applied to all resources created as part of this module"
+  type        = map(string)
+  default     = {}
+}
+
+variable "name_component" {
+  default     = "adb"
+  description = "Component Name - should/will be used in conventional resource naming. Typically this will be a logical name for this part of the system i.e. `API` || `middleware` or more generic like `Billing`"
+  type        = string
+}
+
+############################################
+# RESOURCE INFORMATION
+############################################
+
+variable "resource_group_location" {
+  type        = string
+  default     = "uksouth"
+  description = "Location of Resource group"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of resource group"
+}
+
+variable "databricks_sku" {
+  type        = string
+  default    = "premium"
+  description = "The SKU to use for the databricks instance"
+ 
+  validation {
+    condition     = can(regex("standard|premium|trial", var.databricks_sku))
+    error_message = "Err: Valid options are 'standard', 'premium' or 'trial'."
+  }
+}
