e99a45977064fa919c9966afced641506be20fad
Satenderrathee <satender.rathee07@gmail.com>
2023-04-17 11:13:11 +0100

    fixed

--- azurerm/modules/azurerm-adb/var.tf
+++ azurerm/modules/azurerm-adb/var.tf
@@ -39,6 +39,7 @@ variable "databricks_sku" {
   }
 }
 
+/*
 ############################################
 # Resource Diagnostic Setting 
 ############################################
@@ -59,3 +60,4 @@ variable "data_platform_log_analytics_workspace_id" {
   type        = string
   description = "The Log Analytics Workspace used for the whole Data Platform."
 }
+*/
