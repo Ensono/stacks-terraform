89c5c60f3ba36eedbbdcf83326ed5f97c1fa0dbb
Satenderrathee <satender.rathee07@gmail.com>
2023-04-17 17:27:12 +0100

    fixed

--- azurerm/modules/azurerm-adb/main.tf
+++ azurerm/modules/azurerm-adb/main.tf
@@ -27,7 +27,7 @@ data "azurerm_monitor_diagnostic_categories" "adb_log_analytics_categories" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "databricks_log_analytics" {
-  count = var.local.is_diagnostic_enabled ? 1 : 0
+  count = local.is_diagnostic_enabled ? 1 : 0
   name                       = var.databricksws_diagnostic_setting_name
   target_resource_id         = azurerm_databricks_workspace.example.id
   log_analytics_workspace_id = var.data_platform_log_analytics_workspace_id
