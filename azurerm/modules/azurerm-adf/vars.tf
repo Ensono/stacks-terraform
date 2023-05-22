9a4f0c6dedaba4536e121acd83bc0ecaf86b2032
“Rishikesh <rishikesh_singh@next.co.uk>
2023-05-22 10:29:17 +0100

    fixed

--- azurerm/modules/azurerm-adf/vars.tf
+++ azurerm/modules/azurerm-adf/vars.tf
@@ -84,7 +84,7 @@ variable "managed_virtual_network_enabled" {
 }
 
 variable "adf_maged_private_enpoints" {
-  type        = list(string)
+  type        = map(string)
   default     = []
   description = "List of Resource Ids for Private endpoint in manged ADF manged identity"
 } 
