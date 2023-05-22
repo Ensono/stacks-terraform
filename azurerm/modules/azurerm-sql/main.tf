15cff3eb329b7059ae118e3cf856cdf64e840a4c
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-05-22 15:33:00 +0100

    tf ftm

--- azurerm/modules/azurerm-sql/main.tf
+++ azurerm/modules/azurerm-sql/main.tf
@@ -37,16 +37,16 @@ resource "azurerm_mssql_firewall_rule" "example_fw_rule" {
 }
 
 resource "azurerm_mssql_database" "example-db" {
-  for_each       = toset(var.sql_db_names)
-  name           = each.key
-  server_id      = azurerm_mssql_server.example.id
-  create_mode    = var.create_mode
-  sample_name    = var.sample_name
-  collation      = var.collation
-  license_type   = var.license_type
-  sku_name       = var.sku_name
-  zone_redundant = var.zone_redundant
+  for_each                    = toset(var.sql_db_names)
+  name                        = each.key
+  server_id                   = azurerm_mssql_server.example.id
+  create_mode                 = var.create_mode
+  sample_name                 = var.sample_name
+  collation                   = var.collation
+  license_type                = var.license_type
+  sku_name                    = var.sku_name
+  zone_redundant              = var.zone_redundant
   auto_pause_delay_in_minutes = var.auto_pause_delay_in_minutes
-  tags           = var.resource_tags
+  tags                        = var.resource_tags
 
 }
