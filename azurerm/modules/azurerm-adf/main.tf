15633e394fd6639ae504c3e27e783614418a3870
“Rishikesh <rishikesh_singh@next.co.uk>
2023-05-22 10:54:57 +0100

    fixed

--- azurerm/modules/azurerm-adf/main.tf
+++ azurerm/modules/azurerm-adf/main.tf
@@ -69,10 +69,10 @@ resource "azurerm_data_factory" "example" {
   }
 }
 
-resource "azurerm_data_factory_managed_private_endpoint" "example" {
-  for_each           = toset(var.adf_maged_private_enpoints)
-  name               = "private-${each.key}"
-  data_factory_id    = azurerm_data_factory.example[0].id
-  target_resource_id = each.key
+# resource "azurerm_data_factory_managed_private_endpoint" "example" {
+#   for_each           = toset(var.adf_maged_private_enpoints)
+#   name               = "private-${each.key}"
+#   data_factory_id    = azurerm_data_factory.example[0].id
+#   target_resource_id = each.key
 
-}
+# }
