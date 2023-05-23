4f508f738bd5e0a6098d337811d7eb48c1a217df
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-05-23 10:42:27 +0100

    Removed version contraints
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- azurerm/modules/azurerm-aks/constraints.tf
+++ azurerm/modules/azurerm-aks/constraints.tf
@@ -2,13 +2,11 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.57"
+
     }
 
     tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
+
     }
   }
 }
