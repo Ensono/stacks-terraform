5017d91f177458b6afd7659c91fae6cd006aec43
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-05-26 09:07:20 +0100

    update depends on

--- azurerm/modules/azurerm-adls/main.tf
+++ azurerm/modules/azurerm-adls/main.tf
@@ -39,8 +39,8 @@ resource "azurerm_storage_container" "storage_container_blob" {
   storage_account_name  = azurerm_storage_account.storage_account_default[each.value.account].name
   container_access_type = var.container_access_type
 
-  depends_on = [azurerm_storage_account.storage_account_default, null_resource.sleep]
-  # depends_on = [azurerm_storage_account.storage_account_default, azurerm_role_assignment.storage_role_context, data.azurerm_client_config.current, null_resource.sleep]
+  # depends_on = [azurerm_storage_account.storage_account_default, null_resource.sleep]
+  depends_on = [azurerm_storage_account.storage_account_default, azurerm_role_assignment.storage_role_context, data.azurerm_client_config.current, null_resource.sleep]
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
@@ -48,8 +48,8 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
   name               = each.key
   storage_account_id = azurerm_storage_account.storage_account_default[each.value.account].id
 
-  depends_on = [azurerm_storage_account.storage_account_default, null_resource.sleep]
-  # depends_on = [azurerm_storage_account.storage_account_default, azurerm_role_assignment.storage_role_context, data.azurerm_client_config.current, null_resource.sleep]
+  # depends_on = [azurerm_storage_account.storage_account_default, null_resource.sleep]
+  depends_on = [azurerm_storage_account.storage_account_default, azurerm_role_assignment.storage_role_context, data.azurerm_client_config.current, null_resource.sleep]
 }
 
 data "azurerm_client_config" "current" {}
