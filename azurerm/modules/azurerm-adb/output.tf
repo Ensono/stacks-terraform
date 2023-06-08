0be934be25d59cd5b7446e81f520227b4e4e787d
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-06-08 14:15:18 +0100

    add index to outputs

--- azurerm/modules/azurerm-adb/output.tf
+++ azurerm/modules/azurerm-adb/output.tf
@@ -14,35 +14,35 @@ output "databricks_hosturl" {
 
 output "databricks_workspace_private_endpoint_connection_workspace_id" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.workspace_id
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].workspace_id
 }
 
 output "databricks_workspace_private_endpoint_connection_private_endpoint_id" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.private_endpoint_id
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].private_endpoint_id
 }
 
 output "databricks_workspace_private_endpoint_connection_name" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.connections.0.name
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].connections.0.name
 }
 
 output "databricks_workspace_private_endpoint_connection_workspace_private_endpoint_id" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.connections.0.workspace_private_endpoint_id
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].connections.0.workspace_private_endpoint_id
 }
 
 output "databricks_workspace_private_endpoint_connection_status" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.connections.0.status
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].connections.0.status
 }
 
 output "databricks_workspace_private_endpoint_connection_description" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.connections.0.description
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].connections.0.description
 }
 
 output "databricks_workspace_private_endpoint_connection_action_required" {
   # count = var.enable_private_network ? 1 : 0
-  value = data.azurerm_databricks_workspace_private_endpoint_connection.example.connections.0.action_required
+  value = data.azurerm_databricks_workspace_private_endpoint_connection.example[0].connections.0.action_required
 }
