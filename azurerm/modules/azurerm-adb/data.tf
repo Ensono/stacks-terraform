b391aeec19fc3edec1552adb21d4e4de75d89fb2
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-06-12 12:55:47 +0100

    comment out data

--- azurerm/modules/azurerm-adb/data.tf
+++ azurerm/modules/azurerm-adb/data.tf
@@ -30,12 +30,12 @@ data "azurerm_subnet" "private_subnet" {
   resource_group_name  = var.vnet_resource_group
 }
 
-data "azurerm_subnet" "pe_subnet" {
-  count                = var.enable_private_network && var.managed_vnet == false ? 1 : 0
-  name                 = var.pe_subnet_name
-  virtual_network_name = var.vnet_name
-  resource_group_name  = var.vnet_resource_group
-}
+# data "azurerm_subnet" "pe_subnet" {
+#   count                = var.enable_private_network && var.managed_vnet == false ? 1 : 0
+#   name                 = var.pe_subnet_name
+#   virtual_network_name = var.vnet_name
+#   resource_group_name  = var.vnet_resource_group
+# }
 
 data "azurerm_databricks_workspace_private_endpoint_connection" "example" {
   count               = var.enable_private_network && var.managed_vnet == false ? 1 : 0
