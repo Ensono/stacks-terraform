a303b862e5f8e0b576561bd527f966473b1a2149
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-06-13 07:47:25 +0100

    updates

--- azurerm/modules/azurerm-adb/network.tf
+++ azurerm/modules/azurerm-adb/network.tf
@@ -117,7 +117,7 @@ resource "azurerm_private_endpoint" "databricks" {
 
   private_dns_zone_group {
     name                 = "private-dns-zone-uiapi"
-    private_dns_zone_ids = [azurerm_private_dns_zone.dns.id]
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns[0].id]
   }
 }
 
@@ -140,8 +140,8 @@ resource "azurerm_private_dns_zone_virtual_network_link" "uiapidnszonevnetlink" 
   count                 = var.enable_private_network && var.managed_vnet == false ? 1 : 0
   name                  = "db-dns-vnet-link"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.dns.name
-  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  private_dns_zone_name = azurerm_private_dns_zone.dns[0].name
+  virtual_network_id    = data.azurerm_virtual_network.vnet[0].id
 }
 
 resource "azurerm_public_ip" "pip" {
