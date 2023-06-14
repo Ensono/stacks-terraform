d154b7e39dbb4acb7a070d04ff86d6de20a1653b
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-06-14 09:45:41 +0100

    add auth pe

--- azurerm/modules/azurerm-adb/network.tf
+++ azurerm/modules/azurerm-adb/network.tf
@@ -154,6 +154,28 @@ resource "azurerm_private_endpoint" "databricks" {
   depends_on = [azurerm_databricks_workspace.example, azurerm_private_dns_zone.dns]
 }
 
+resource "azurerm_private_endpoint" "auth" {
+  name                = "${var.resource_namer}-pe-databricks-auth"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.create_pe_subnet ? azurerm_subnet.pe_subnet[0].id : data.azurerm_subnet.pe_subnet[0].id
+
+  private_service_connection {
+    name                           = "${var.resource_namer}-db-pe-auth"
+    private_connection_resource_id = azurerm_databricks_workspace.example.id
+    is_manual_connection           = false
+    subresource_names              = ["browser_authentication"]
+  }
+
+  private_dns_zone_group {
+    name                 = "databricks_auth"
+    private_dns_zone_ids = [azurerm_private_dns_zone.dns[0].id]
+  }
+
+  depends_on = [azurerm_databricks_workspace.example, azurerm_private_dns_zone.dns]
+
+}
+
 resource "azurerm_private_dns_zone" "dns" {
   count               = var.enable_private_network && var.managed_vnet == false ? 1 : 0
   name                = "privatelink.azuredatabricks.net"
