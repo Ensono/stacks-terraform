0f969e1d11b10e8df6ac9f91f71e442da893d27b
Trishisingh <rishisingh901@gmail.com>
2023-06-20 12:49:45 +0100

    updated

--- azurerm/modules/azurerm-vmss/ci_cd_tool_install.sh
+++ azurerm/modules/azurerm-vmss/ci_cd_tool_install.sh
@@ -14,7 +14,17 @@ rm packages-microsoft-prod.deb
 # Update the list of packages after we added packages.microsoft.com
 sudo apt-get update
 # Install Docker
-curl -sL https://test.docker.com | sudo bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#curl -sL https://test.docker.com | sudo bash
 # Install PowerShell
 sudo apt-get install -y powershell
 # Install Modules
