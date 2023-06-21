1d9d78fed41beb54f1136e938a9a48a13650820d
Trishisingh <rishisingh901@gmail.com>
2023-06-21 09:39:35 +0100

    updated-new-sizexs-updated

--- azurerm/modules/azurerm-vmss/ci_cd_tool_install.sh
+++ azurerm/modules/azurerm-vmss/ci_cd_tool_install.sh
@@ -23,6 +23,7 @@ echo \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
 
 # Install Docker
 #sudo apt-get update
