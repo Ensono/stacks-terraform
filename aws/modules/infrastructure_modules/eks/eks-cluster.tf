4a26ac5f27d07e4de4b82c692cb3b982207d0d67
William Ayerst <11176822+williamayerst@users.noreply.github.com>
2023-07-03 15:56:02 +0100

    updates BR var

--- aws/modules/infrastructure_modules/eks/eks-cluster.tf
+++ aws/modules/infrastructure_modules/eks/eks-cluster.tf
@@ -53,7 +53,7 @@ data "aws_ami" "eks_default_bottlerocket" {
 
   filter {
     name   = "name"
-    values = ["bottlerocket-aws-k8s-${local.cluster_version}-x86_64-*"]
+    values = ["bottlerocket-aws-k8s-${var.cluster_version}-x86_64-*"]
   }
 }
 
