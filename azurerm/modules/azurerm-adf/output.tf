111d1d580178eb08e11c5fab932f8569f8f54140
RhysBushnell <32571778+RhysBushnell@users.noreply.github.com>
2023-07-05 12:37:26 +0100

    add adf runtime output (#69)

--- azurerm/modules/azurerm-adf/output.tf
+++ azurerm/modules/azurerm-adf/output.tf
@@ -11,3 +11,8 @@ output "adf_managed_identity" {
   description = "Azure Data Factory Name"
   value       = azurerm_data_factory.example.0.identity[0].principal_id
 }
+
+output "adf_integration_runtime_name" {
+  description = "The Integration Runtime using VNET"
+  value       = var.managed_virtual_network_enabled ? azurerm_data_factory_integration_runtime_azure.example[0].name : "AutoResolveIntegrationRuntime"
+}
