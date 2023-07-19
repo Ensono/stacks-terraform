768f763b10a42bdde1e3d22488686bcbc972c4aa
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-07-19 14:16:48 +0100

    remove databricks resources from module

--- azurerm/modules/azurerm-adb/constraints.tf
+++ azurerm/modules/azurerm-adb/constraints.tf
@@ -5,9 +5,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    #TODO: note this is just added right now without any use, can be used In future for databricks provider
-    databricks = {
-      source = "databricks/databricks"
-    }
   }
 }
