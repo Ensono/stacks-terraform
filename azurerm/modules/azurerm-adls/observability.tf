9cd8a0bcdf5299d5b245be1b95717671ade5682a
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-07-21 14:05:39 +0100

    update adls

--- azurerm/modules/azurerm-adls/observability.tf
+++ azurerm/modules/azurerm-adls/observability.tf
@@ -1,12 +1,21 @@
 data "azurerm_monitor_diagnostic_categories" "adls_log_analytics_categories" {
-  for_each    = var.storage_account_details && var.la_workspace_id != ""
-  resource_id = azurerm_storage_account.storage_account_default["${each.value.name}"].id
+  for_each = {
+    for account_name, account_details in var.storage_account_details : account_name => account_details
+    if var.la_workspace_id != ""
+  }
+  resource_id = azurerm_storage_account.storage_account_default["${each.key}"].id
+
+  depends_on = [azurerm_storage_account.storage_account_default]
 }
 
 resource "azurerm_monitor_diagnostic_setting" "adls_log_analytics" {
-  for_each                       = var.storage_account_details && var.la_workspace_id != ""
+  for_each = {
+    for account_name, account_details in var.storage_account_details : account_name => account_details
+    if var.la_workspace_id != ""
+  }
+
   name                           = "Storage to Log Analytics"
-  target_resource_id             = azurerm_storage_account.storage_account_default["${each.value.name}"].id
+  target_resource_id             = azurerm_storage_account.storage_account_default["${each.key}"].id
   log_analytics_workspace_id     = var.la_workspace_id
   log_analytics_destination_type = "Dedicated"
 
@@ -37,4 +46,6 @@ resource "azurerm_monitor_diagnostic_setting" "adls_log_analytics" {
       }
     }
   }
+
+  depends_on = [data.azurerm_monitor_diagnostic_categories.adls_log_analytics_categories]
 }
