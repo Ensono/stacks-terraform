3883caaf1e73a9c47b67bcaa49e6196bc8c79ea3
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-07-21 13:53:41 +0100

    add index

--- azurerm/modules/azurerm-kv/observability.tf
+++ azurerm/modules/azurerm-kv/observability.tf
@@ -1,6 +1,6 @@
 data "azurerm_monitor_diagnostic_categories" "kv_log_analytics_categories" {
   count       = var.la_workspace_id != "" ? 1 : 0
-  resource_id = azurerm_key_vault.example.id
+  resource_id = azurerm_key_vault.example[0].id
 
   depends_on = [azurerm_key_vault]
 }
@@ -8,7 +8,7 @@ data "azurerm_monitor_diagnostic_categories" "kv_log_analytics_categories" {
 resource "azurerm_monitor_diagnostic_setting" "kv_log_analytics" {
   count                          = var.la_workspace_id != "" ? 1 : 0
   name                           = "KV to Log Analytics"
-  target_resource_id             = azurerm_key_vault.example.id
+  target_resource_id             = azurerm_key_vault.example[0].id
   log_analytics_workspace_id     = var.la_workspace_id
   log_analytics_destination_type = "Dedicated"
 
