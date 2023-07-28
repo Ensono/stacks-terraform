2e56b1add29d92306f4785a6f12dc90bf813b31f
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-07-28 11:21:35 +0100

    fix var name

--- azurerm/modules/azurerm-adb/network.tf
+++ azurerm/modules/azurerm-adb/network.tf
@@ -137,7 +137,7 @@ resource "azurerm_private_endpoint" "databricks" {
   name                = "${var.resource_namer}-pe-databricks"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.create_pe_subnet ? azurerm_subnet.pe_subnet[0].id : pe_subnet_id
+  subnet_id           = var.create_pe_subnet ? azurerm_subnet.pe_subnet[0].id : var.pe_subnet_id
 
   private_service_connection {
     name                           = "${var.resource_namer}-db-pe"
