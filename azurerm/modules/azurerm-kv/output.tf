0ca2ff872b6c548b0a925146a33cd4906cd6bad8
Satenderrathee <satender.rathee07@gmail.com>
2023-09-12 16:23:47 +0100

    updated

--- azurerm/modules/azurerm-kv/output.tf
+++ azurerm/modules/azurerm-kv/output.tf
@@ -11,7 +11,7 @@ output "vault_uri" {
 output "key_vault_name" {
   value = var.create_kv ? azurerm_key_vault.example.0.name : ""
 }
-output "private_endpoint_id" {
-  description = "The ID of the Private Endpoint associated with the Key Vault."
-  value       = azurerm_private_endpoint.pe[0].id
-}
+# output "private_endpoint_id" {
+#   description = "The ID of the Private Endpoint associated with the Key Vault."
+#   value       = azurerm_private_endpoint.pe[0].id
+# }
