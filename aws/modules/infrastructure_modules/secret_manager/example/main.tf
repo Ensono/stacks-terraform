879797d9bc948ccc30be5e7c5c6b4335213364b1
Bal Purewal <baldip.purewal@ensono.com>
2023-11-15 15:05:01 +0000

    applied terraform formatting

--- aws/modules/infrastructure_modules/secret_manager/example/main.tf
+++ aws/modules/infrastructure_modules/secret_manager/example/main.tf
@@ -2,8 +2,8 @@ module "secret_manager" {
   source = "../"
 
   secrets = [{
-    name                    = "mysecret"
-    description             = "secret description"
-    secret_string           = "this is the plain text secret"
+    name          = "mysecret"
+    description   = "secret description"
+    secret_string = "this is the plain text secret"
   }]
 }
