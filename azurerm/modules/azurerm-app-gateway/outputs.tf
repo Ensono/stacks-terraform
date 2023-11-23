a4bff8b53a3f37b633eae8b485dab5514a7f31d5
Jack Blower <Jack@elvenspellmaker.co.uk>
2023-11-23 16:35:31 +0000

    fix: ff

--- azurerm/modules/azurerm-app-gateway/outputs.tf
+++ azurerm/modules/azurerm-app-gateway/outputs.tf
@@ -9,12 +9,12 @@ output "app_gateway_name" {
 }
 
 output "app_gateway_ip" {
-  description = "Application Gateway public IP. Should be used with DNS provider at a top level. Can have multiple subs pointing to it - e.g. app.sub.domain.com, app-uat.sub.domain.com. App Gateway will perform SSL termination for all "
+  description = "Application Gateway public IP. Should be used with DNS provider at a top level. Can have multiple subs pointing to it - e.g. app.sub.domain.com, app-uat.sub.domain.com. App Gateway will perform SSL termination for all"
   value       = data.azurerm_public_ip.default.ip_address
 }
 
 output "app_gateway_ip_name" {
-  description = "Application Gateway public IP name. Should be used with DNS provider at a top level. Can have multiple subs pointing to it - e.g. app.sub.domain.com, app-uat.sub.domain.com. App Gateway will perform SSL termination for all "
+  description = "Application Gateway public IP name"
   value       = azurerm_public_ip.app_gateway.name
 }
 
