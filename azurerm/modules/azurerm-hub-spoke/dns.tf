f89d3899ba14def447469df09a3f065b327239be
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2023-11-27 19:18:20 +0000

    Prevent DNS zones from being created if private networking is not enabled
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- azurerm/modules/azurerm-hub-spoke/dns.tf
+++ azurerm/modules/azurerm-hub-spoke/dns.tf
@@ -1,13 +1,16 @@
+
+# Creates private DNS zones if private networks have been enabled
 resource "azurerm_private_dns_zone" "example" {
-  for_each            = length(local.hub_resource_group_name) > 0 ? toset(var.dns_zone_name) : toset([])
+  for_each            = var.enable_private_networks ? toset(var.dns_zone_name) : toset([])
   name                = each.key
   resource_group_name = local.hub_resource_group_name[0]
   tags                = var.tags
   depends_on          = [azurerm_resource_group.network]
 }
 
+# Creates links for the virtual network in private DNS zone if private networks have been enabled
 resource "azurerm_private_dns_zone_virtual_network_link" "hub-privatelink-dns" {
-  for_each              = var.link_dns_network == true ? toset(var.dns_zone_name) : toset([])
+  for_each              = var.enable_private_networks && var.link_dns_network == true ? toset(var.dns_zone_name) : toset([])
   name                  = each.key
   resource_group_name   = local.hub_resource_group_name[0]
   registration_enabled  = false # Auto registration_enabled set to false as we cannot add multiple Private DNS to 1 Vnet
