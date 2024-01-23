0f444f43d11c987deb63c7ec6d85dd20e9f2d316
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2024-01-23 16:04:30 +0000

    Removed deprecated argument
    Signed-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- azurerm/modules/azurerm-adb/network.tf
+++ azurerm/modules/azurerm-adb/network.tf
@@ -54,7 +54,7 @@ resource "azurerm_subnet" "pe_subnet" {
   resource_group_name                            = var.vnet_resource_group
   virtual_network_name                           = var.vnet_name
   address_prefixes                               = var.pe_subnet_prefix
-  enforce_private_link_endpoint_network_policies = true
+  private_endpoint_network_policies_enabled      = true
 }
 
 
