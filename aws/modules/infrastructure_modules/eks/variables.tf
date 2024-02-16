380a7cb019a27e20b3f6fb34270d9ccc3492719f
Zaid Khider <zaid.khider@kingfisher.com>
2024-02-16 11:07:19 +0000

    create new file for validation

--- aws/modules/infrastructure_modules/eks/variables.tf
+++ aws/modules/infrastructure_modules/eks/variables.tf
@@ -107,12 +107,3 @@ variable "cis_bootstrap_image" {
   type        = string
   default     = ""
 }
-
-resource "null_resource" "cis_bootstrap_validation" {
-  lifecycle {
-    precondition {
-      condition     = (var.enable_cis_bootstrap == true && var.cis_bootstrap_image != "") || var.enable_cis_bootstrap == false
-      error_message = "cis_bootstrap_image must be set if enable_cis_bootstrap is true"
-    }
-  }
-}
