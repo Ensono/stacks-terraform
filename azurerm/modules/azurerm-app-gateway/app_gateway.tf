43cbf96d357aa0fc1b3cfe93c9b33bf68ddad07e
Bal Purewal <baldip.purewal@ensono.com>
2024-10-09 17:32:24 +0100

    added sku to public ip

--- azurerm/modules/azurerm-app-gateway/app_gateway.tf
+++ azurerm/modules/azurerm-app-gateway/app_gateway.tf
@@ -19,6 +19,7 @@ resource "azurerm_public_ip" "app_gateway" {
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
   allocation_method   = "Dynamic"
+  sku                 = "Standard"
   lifecycle {
     ignore_changes = [
       tags,
