1d99843424b033e7790b172b7cdaad01e7ad7eb2
Szymon Marciniak <szymon.marciniak@ensono.com>
2025-07-09 12:36:20 +0200

    add partition data resource

--- aws/modules/infrastructure_modules/eks/data.tf
+++ aws/modules/infrastructure_modules/eks/data.tf
@@ -2,6 +2,8 @@ data "aws_caller_identity" "this" {}
 
 data "aws_availability_zones" "available" {}
 
+data "aws_partition" "current" {}
+
 ## EKS
 data "aws_iam_policy_document" "eks_secret_encryption_kms_key_policy" {
   statement {
